0ba1dc93fffaac94c300560e08114a3b17c93456
tumtpk <pk.prateep@gmail.com>
2018-08-22 10:37:03 +0700

    init

---
+++ junittest/src/main/java/com/javacode/examples/junittest/Calculate.java
@@ -0,0 +1,13 @@
+package com.javacode.examples.junittest;
+
+public class Calculate {
+	
+	public boolean Input(int score) {
+		return true;
+	}
+	
+	public String grade(int score) {
+		return null;
+	}
+	
+}
